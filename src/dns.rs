b94351a73c3165c88ce2e1bc8744c7e6ddbfae53
timog <timo.glane@gmail.com>
2022-03-15 22:15:11 +0100

    feat: Added missing dns recorddata types

--- src/dns.rs
+++ src/dns.rs
@@ -136,7 +136,13 @@ pub enum RecordType {
     A = 1,
     NS = 2,
     CNAME = 5,
+    SOA = 6,
+    NULL = 10,
+    WKS = 11,
     PTR = 12,
+    HINFO = 13,
+    MINFO = 14,
+    MX = 15,
     TXT = 16,
     AAAA = 28,
     SRV = 33,
@@ -148,7 +154,13 @@ impl RecordType {
              1 => RecordType::A,
              2 => RecordType::NS,
              5 => RecordType::CNAME,
+             6 => RecordType::SOA,
+            10 => RecordType::NULL,
+            11 => RecordType::WKS,
             12 => RecordType::PTR,
+            13 => RecordType::HINFO,
+            14 => RecordType::MINFO,
+            15 => RecordType::MX,
             16 => RecordType::TXT,
             28 => RecordType::AAAA,
             33 => RecordType::SRV,
@@ -161,7 +173,13 @@ impl RecordType {
             RecordType::A => false,
             RecordType::NS => true,
             RecordType::CNAME => true,
+            RecordType::SOA => true,
+            RecordType::NULL => false,
+            RecordType::WKS => false,
             RecordType::PTR => true,
+            RecordType::HINFO => false,
+            RecordType::MINFO => true,
+            RecordType::MX => true,
             RecordType::TXT => true,
             RecordType::AAAA => false,
             RecordType::SRV => true,
@@ -173,18 +191,44 @@ impl RecordType {
 
 #[derive(Clone, Debug)]
 pub enum RecordData {
-    // TODO Add missing record types
     A(Ipv4Addr),
     NS(String),
     CNAME(String),
+    SOA {
+        mname: String,
+        rname: String,
+        serial: u32,
+        refresh: u32,
+        retry: u32,
+        expire: u32,
+        minimum: u32,
+    },
+    NULL(Vec<u8>),
+    WKS {
+        address: u32,
+        protocol: u8,
+        bitmap: Vec<u8> // TODO Better representation for the bitmap
+    },
     PTR(String),
+    HINFO {
+        cpu: Vec<u8>,
+        os: Vec<u8>,
+    },
+    MINFO {
+        rmailbx: String,
+        emailbx: String,
+    },
+    MX {
+        preference: u16,
+        exchange: String,
+    },
     TXT(Vec<String>),
     AAAA(Ipv6Addr),
     SRV {
         priority: u16,
         weight: u16,
         port: u16,
-        target: String
+        target: String,
     },
     Raw(Vec<u8>)
 }
@@ -195,7 +239,13 @@ impl RecordData {
             RecordType::A => RecordData::A(Ipv4Addr::from(u32::from_be_bytes(buffer.try_into().unwrap()))),
             RecordType::NS => RecordData::NS(from_fqdn(buffer).0),
             RecordType::CNAME => RecordData::CNAME(from_fqdn(buffer).0),
+            RecordType::SOA => RecordData::parse_soa(buffer),
+            RecordType::NULL => RecordData::NULL(buffer.to_vec()),
+            RecordType::WKS => RecordData::parse_wks(buffer),
             RecordType::PTR => RecordData::PTR(from_fqdn(buffer).0),
+            RecordType::HINFO => RecordData::parse_hinfo(buffer),
+            RecordType::MINFO => RecordData::parse_minfo(buffer),
+            RecordType::MX => RecordData::parse_mx(buffer),
             RecordType::TXT => RecordData::parse_txt(buffer),
             RecordType::AAAA => RecordData::AAAA(Ipv6Addr::from(u128::from_be_bytes(buffer.try_into().unwrap()))),
             RecordType::SRV => RecordData::parse_srv(buffer),
@@ -203,7 +253,45 @@ impl RecordData {
         }
     }
 
-    fn parse_txt(buffer: &[u8]) -> RecordData {
+    fn parse_soa(buffer: &[u8]) -> Self {
+        let (mname, idx_advanced) = from_fqdn(buffer);
+        let (rname, idx_advanced) = from_fqdn(&buffer[idx_advanced..]);
+        let serial = u32::from_be_bytes(buffer[idx_advanced..idx_advanced+4].try_into().unwrap());
+        let refresh = u32::from_be_bytes(buffer[idx_advanced+4..idx_advanced+8].try_into().unwrap());
+        let retry = u32::from_be_bytes(buffer[idx_advanced+8..idx_advanced+12].try_into().unwrap());
+        let expire = u32::from_be_bytes(buffer[idx_advanced+12..idx_advanced+16].try_into().unwrap());
+        let minimum = u32::from_be_bytes(buffer[idx_advanced+16..idx_advanced+20].try_into().unwrap());
+        RecordData::SOA { mname, rname, serial, refresh, retry, expire, minimum }
+    }
+
+    fn parse_wks(buffer: &[u8]) -> Self {
+        let address = u32::from_be_bytes(buffer[0..2].try_into().unwrap());
+        let protocol = buffer[2];
+        let bitmap = buffer[3..].to_vec(); // TODO Improve bitmap representation
+        RecordData::WKS { address, protocol, bitmap }
+    }
+
+    fn parse_hinfo(buffer: &[u8]) -> Self {
+        let cpu_len = buffer[0] as usize;
+        let cpu = buffer[1..cpu_len].to_vec();
+        let os_len = buffer[cpu_len+ 1] as usize;
+        let os = buffer[cpu_len+2..cpu_len+2+os_len].to_vec();
+        RecordData::HINFO { cpu, os }
+    }
+
+    fn parse_minfo(buffer: &[u8]) -> Self {
+        let (rmailbx, idx_advanced) = from_fqdn(buffer);
+        let (emailbx, _) = from_fqdn(&buffer[idx_advanced..]);
+        RecordData::MINFO { rmailbx, emailbx }
+    }
+
+    fn parse_mx(buffer: &[u8]) -> Self {
+        let preference = u16::from_be_bytes(buffer[0..2].try_into().unwrap());
+        let exchange = from_fqdn(&buffer[2..]).0;
+        RecordData::MX { preference, exchange }
+    }
+
+    fn parse_txt(buffer: &[u8]) -> Self {
         let mut txt_store = Vec::<String>::new();
         let mut idx = 0;
         while idx < buffer.len() {
@@ -214,7 +302,7 @@ impl RecordData {
         RecordData::TXT(txt_store)
     }
 
-    fn parse_srv(buffer: &[u8]) -> RecordData {
+    fn parse_srv(buffer: &[u8]) -> Self {
         let priority = u16::from_be_bytes(buffer[0..2].try_into().unwrap());
         let weight = u16::from_be_bytes(buffer[2..4].try_into().unwrap());
         let port = u16::from_be_bytes(buffer[4..6].try_into().unwrap());
@@ -229,7 +317,15 @@ impl ByteConvertible for RecordData {
             RecordData::A(_) => 4,
             RecordData::NS(ref name) => name.len() + 2,
             RecordData::CNAME(ref name) => name.len() + 2,
+            RecordData::SOA { ref mname, ref rname, serial: _, refresh: _, retry: _, expire: _, minimum: _ } => {
+                mname.len() + 2 + rname.len() + 2 + 4 + 4 + 4 + 4 + 4
+            },
+            RecordData::NULL(ref buffer) => buffer.len(),
+            RecordData::WKS { address: _, protocol: _, ref bitmap } => 4 + 1 + bitmap.len(),
             RecordData::PTR(ref name) => name.len() + 2,
+            RecordData::HINFO { ref cpu, ref os } => 2 + cpu.len() + os.len(),
+            RecordData::MINFO { ref rmailbx, ref emailbx } => rmailbx.len() + 2 + emailbx.len() + 2,
+            RecordData::MX { preference: _, ref exchange } => 2 + exchange.len() + 2,
             RecordData::TXT(ref store) => store.iter().fold(0, |acc, elem| acc + elem.len() + 1),
             RecordData::AAAA(_) => 16,
             RecordData::SRV { priority: _, weight: _, port: _, ref target } => 2 + 2 + 2 + target.len() + 2,
@@ -241,9 +337,47 @@ impl ByteConvertible for RecordData {
     fn to_bytes(&self) -> Vec<u8> {
         let buff = match self {
             RecordData::A(ref buffer) => buffer.octets().to_vec(),
-            RecordData::NS(ref name) => to_fqdn(name),
-            RecordData::CNAME(ref name) => to_fqdn(name),
+            RecordData::NS(ref name) => to_fqdn(&name),
+            RecordData::CNAME(ref name) => to_fqdn(&name),
+            RecordData::SOA { ref mname, ref rname, ref serial, ref refresh, ref retry, ref expire, ref minimum  } => {
+                let mut buffer = to_fqdn(&mname);
+                buffer.extend_from_slice(&to_fqdn(&rname));
+                buffer.extend_from_slice(&u32::to_be_bytes(*serial));
+                buffer.extend_from_slice(&u32::to_be_bytes(*refresh));
+                buffer.extend_from_slice(&u32::to_be_bytes(*retry));
+                buffer.extend_from_slice(&u32::to_be_bytes(*expire));
+                buffer.extend_from_slice(&u32::to_be_bytes(*minimum));
+                buffer
+            },
+            RecordData::NULL(ref buffer) => buffer.clone(),
+            RecordData::WKS { ref address, ref protocol, ref bitmap } => {
+                let mut buffer = Vec::with_capacity(4 + 1 + bitmap.len());
+                buffer.extend_from_slice(&u32::to_be_bytes(*address));
+                buffer.extend_from_slice(&u8::to_be_bytes(*protocol));
+                buffer.extend_from_slice(&bitmap);
+                buffer
+            },
             RecordData::PTR(ref name) => to_fqdn(name),
+            RecordData::HINFO { ref cpu, ref os } => {
+                let mut buffer = Vec::with_capacity(2 + cpu.len() + os.len());
+                buffer.push(cpu.len() as u8);
+                buffer.extend_from_slice(&cpu);
+                buffer.push(os.len() as u8);
+                buffer.extend_from_slice(&os);
+                buffer
+            }
+            RecordData::MINFO { ref rmailbx, ref emailbx } => {
+                let mut buffer = Vec::with_capacity(rmailbx.len() + 2 + emailbx.len() + 2);
+                buffer.extend_from_slice(&to_fqdn(&rmailbx));
+                buffer.extend_from_slice(&to_fqdn(&emailbx));
+                buffer
+            }
+            RecordData::MX { ref preference, ref exchange } => {
+                let mut buffer = Vec::with_capacity(2 + exchange.len() + 2);
+                buffer.extend_from_slice(&u16::to_be_bytes(*preference));
+                buffer.extend_from_slice(&to_fqdn(&exchange));
+                buffer
+            },
             RecordData::TXT(ref store) => store.iter().fold(Vec::new(), |mut buff, elem| {
                 let txt_bin = elem.as_bytes();
                 buff.push(txt_bin.len() as u8);
@@ -256,7 +390,7 @@ impl ByteConvertible for RecordData {
                 buff.extend_from_slice(&u16::to_be_bytes(*priority));
                 buff.extend_from_slice(&u16::to_be_bytes(*weight));
                 buff.extend_from_slice(&u16::to_be_bytes(*port));
-                buff.extend_from_slice(&to_fqdn(target));
+                buff.extend_from_slice(&to_fqdn(&target));
                 buff
             },
             _ => Vec::new()
